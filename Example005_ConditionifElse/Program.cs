3a7031c2fb12981888a18e83aa8583f805201a41
Huseyn Motsuev <h.motsuev_01@mail.ru>
2022-01-07 17:50:10 +0300

    add Example 005 Condition if-Else

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "хусейн")
+{
+    Console.WriteLine("Ура, это же ХУСЕЙН!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
